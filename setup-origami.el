9f016d3d0a01dbb56c8a98194f8451160e91dd07
Rolando Flores <rolo@tutamail.com>
2023-02-14 14:46:17 -0600

    quité setup-origami

--- setup-origami.el
+++
@@ -1,8 +0,0 @@
-(add-hook 'origami-mode-hook
-          '(lambda()
-             (define-key origami-mode-map (kbd "--f") 'origami-recursively-toggle-node)
-             (define-key origami-mode-map (kbd "--F") 'origami-toggle-all-nodes)
-             (define-key origami-mode-map (kbd "--o") 'origami-open-node-recursively)
-             ))
-
-(provide 'setup-origami)
