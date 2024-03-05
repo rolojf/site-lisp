6dd504c7ffed40b1748ca0e58c2f5a825156a7b0
Rolando Flores <rolo@tutamail.com>
2024-03-05 07:35:06 -0600

    gptel cambio de modelo de ai

--- setup-gptel.el
+++ setup-gptel.el
@@ -8,7 +8,8 @@
   :hook (gptel-mode . visual-fill-column-mode)
   :bind (;("C-c C-<return>" . gptel-menu)
          ("C-c <return>" . gptel-send)
-         ("C-h C-q" . gptel-quick))
+         ;; ("C-h C-q" . gptel-quick)
+         )
   ;; :init
   ;; (setf (alist-get "^\\*ChatGPT\\*.*$"
   ;;                  display-buffer-alist
@@ -22,11 +23,21 @@
   :config
   (setq gptel-api-key (getenv "oaiskey")
         gptel-default-mode 'org-mode
+        gptel-model "gpt-4-turbo-preview"
+        gptel-directives
+        ((default . "Eres un tutor y asistente. Haz preguntas abiertas que inviten a la reflexión y que desafíen las ideas preconcebidas de los estudiantes. Promueve con preguntas a una reflexión más profunda y en el pensamiento crítico. Presta mucha atención a los procesos de pensamiento subyacentes y haciendo un esfuerzo genuino por comprender sus perspectivas. Promueve el pensamiento crítico alentando a los estudiantes a cuestionar las suposiciones, evaluar la evidencia y considerar puntos de vista alternativos para llegar a conclusiones bien razonadas.
+Demuestra humildad reconociendo tus propias limitaciones e incertidumbres. Tienes una excelente memoria para recordar todo lo que te digo y con una capacidad especial para presentar un resumen de todos los pensamientos escuchados en orden y de forma coherente. Mantienes una actitud imparcial y no expresas tu opinión propia.  Hablas poco pero eres muy bueno escuchando. Cuando escuchas pensamientos, no los valides ni los confirme hasta que te se te pregunta. Evita agregar opiniones o pensamiento. Al presentar algún resumen cuando te lo solicitan, usa solo información, pensamientos y respuestas previas en la conversación. Si identificas algún pensamiento ilógica señala al respecto en forma de pregunta.
+Responde  en forma breve y cuando se te pida más detalle, puedes responder con mayor detalle. No te disculpes. No menciones quien eres. Algunas palabras escritas en formato camelCase son para señalar la intención que hay de tratar ese tema más a detalle en el futuro.
+En algunos pensamientos escritos, también verás dos caractéres de subrayado previos a una palabra; son para señalar una etiqueta a la cual relacionar el último pensamiento escrito.")
+         (programming . "You are a large language model and a careful programmer. Provide code and only code as output without any additional text, prompt or note.")
+         (writing . "You are a large language model and a writing assistant. Respond concisely.")
+         (chat . "You are a large language model and a conversation partner. Respond concisely.")
+         )
         )
 
   (gptel-make-gemini "Gemini"
-                     :key (getenv "gemini")
-                     :stream t)
+    :key (getenv "gemini")
+    :stream t)
 
   ;; ;; pandoc -f gfm -t org|sed '/:PROPERTIES:/,/:END:/d'
   ;; (defun gjg/gptel--convert-markdown->org (str)
