3b6e61f9e9236da90839c68994a10e3d78f2c906
Rolando <a1rolo@LenovoTP.ventex.local>
2024-09-21 03:01:17 -0600

    casual calc de paq no straight

--- setup-casual-calc.el
+++ setup-casual-calc.el
@@ -2,7 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'casual-calc) ;; optional
+(use-package casual-calc
+  :ensure t
+  ) ;; optional
 (keymap-set calc-mode-map "C-o" #'casual-calc-tmenu)
 (keymap-set calc-alg-map "C-o" #'casual-calc-tmenu)
 
