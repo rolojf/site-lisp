4520e0e69000d9ba6b8ac44571a48e4b86117845
Rolando <a1rolo@LenovoTP.ventex.local>
2025-07-22 19:44:12 -0600

    agregando gleam

---
+++ setup-gleam.el
@@ -0,0 +1,13 @@
+;;; setup-gleam.el --- Insert description here -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+
+(use-package gleam-ts-mode
+  :ensure t
+  :mode (rx ".gleam" eos))
+
+(provide 'setup-gleam)
+
+;;; setup-gleam.el ends here
