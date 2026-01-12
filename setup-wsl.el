0da783dd3a488f2c467bbb0bcd6c3c8d55d83dc1
Rolando Flores <rolando@psolar.mx>
2026-01-12 11:18:13 -0600

    clean up gptel from use-package and fix copy-paste on windows

--- setup-wsl.el
+++ setup-wsl.el
@@ -5,15 +5,21 @@
 (defun wsl-wayland-copy (start end)
   "Copy the selected region to the Wayland clipboard asynchronously."
   (interactive "r")
-  ;; call-process-on-region arguments:
-  ;; START END: The region to send to the command's stdin.
-  ;; "wl-copy": The program to execute.
-  ;; nil: Do not delete the region after copying.
-  ;; nil: Do not redirect the command's output to a buffer.
-  ;; nil: Do not display the output.
-  (call-process-region start end "wl-copy" nil nil nil))
+  (call-process-region start end "wl-copy" nil nil nil)
+  (deactivate-mark) ;; Deactivates the region
+  (goto-char start)) ;; Moves point back to the start of the selection
 
 (global-set-key (kbd "M-w") 'wsl-wayland-copy)
+
+(defun wsl-wayland-paste ()
+  "Paste from Wayland clipboard enforcing UTF-8 encoding."
+  (interactive)
+  ;; Force Emacs to read the shell output as UTF-8
+  (let ((coding-system-for-read 'utf-8))
+    ;; Insert the output of wl-paste
+    (insert (shell-command-to-string "wl-paste --no-newline"))))
+
+(global-set-key (kbd "C-y") 'wsl-wayland-paste)
 
 (provide 'setup-wsl)
 ;;; setup-wsl.el ends here
