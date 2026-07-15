2d1124b61f0c5d839b1277327bd633143b00fac0
Sprite <noreply@sprites.dev>
2026-07-15 06:34:17 +0000

    cambio quitando agent-shell

--- setup-agents.el
+++ setup-agents.el
@@ -2,21 +2,23 @@
 ;;; Commentary:
 ;;; Code:
 
-(require-package 'acp)
-(require-package 'shell-maker)
-(require-package 'agent-shell)
-(with-eval-after-load 'agent-shell
-  (setq agent-shell-anthropic-authentication (agent-shell-anthropic-make-authentication :login t)
-        agent-shell-file-completion-enabled t
-        agent-shell-prefer-viewport-interaction t
-        ))
+;; (require-package 'acp)
+;; (require-package 'shell-maker)
+;; (require-package 'agent-shell)
+;; (with-eval-after-load 'agent-shell
+;;  (setq agent-shell-anthropic-authentication (agent-shell-anthropic-make-authentication :login t)
+;;         agent-shell-file-completion-enabled t
+;;        agent-shell-prefer-viewport-interaction t
+;;         ))
 
-(global-set-key (kbd "C-c s") #'agent-shell)
-(global-unset-key (kbd "M-Z"))
-
-
+;; (global-set-key (kbd "C-c s") #'agent-shell)
+;; (global-unset-key (kbd "M-Z"))
 ;; claude plugins marketplaces update xenodium-emacs-skills
 ;; para actualizar skills
+
+(require-package 'pi-coding-agent)
+(defalias 'pi 'pi-coding-agent)
+
 
 (provide 'setup-agents)
 ;;; setup-agents.el ends here
