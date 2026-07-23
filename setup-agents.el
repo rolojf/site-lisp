5ba5764d720dd6bf1807ac93e0e13abc3d6f7876
Rolando Flores <rolando@psolar.mx>
2026-07-22 23:37:43 -0600

    cambio a los agentes

--- setup-agents.el
+++ setup-agents.el
@@ -20,17 +20,37 @@
 
 (defalias 'pi 'pi-coding-agent)
 
+;; (with-eval-after-load 'tramp
+;;   (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
+
 (with-eval-after-load 'tramp
-  (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
+  (setq tramp-remote-path
+        '("/.sprite/languages/node/nvm/versions/node/v24.18.0/bin"
+          "/.sprite/languages/go/current/bin"
+          "/.sprite/languages/ruby/rbenv/bin"
+          "/.sprite/languages/ruby/rbenv/shims"
+          "/.sprite/languages/rust/cargo/bin"
+          "/.sprite/languages/bun/bin"
+          "/.sprite/languages/deno/bin"
+          "/.sprite/languages/python/pyenv/bin"
+          "/.sprite/languages/python/pyenv/shims"
+          "/home/sprite/.local/bin"
+          tramp-default-remote-path
+          "/.sprite/bin")))
 
 (defconst my-sprite-tercero-path
-  "/ssh:sprite@localhost#3000:/home/sprite/"
+  "/ssh:sprite@localhost#4000:/home/sprite/"
   "Ruta TRAMP al Sprite tercero.")
 
 (defun my-tercero ()
   "Abrir el directorio principal del Sprite tercero."
   (interactive)
   (find-file my-sprite-tercero-path))
+
+(with-eval-after-load 'pi-coding-agent-ui
+  (setq pi-coding-agent-executable
+        '("/.sprite/languages/node/nvm/versions/node/v24.18.0/bin/node"
+          "/.sprite/languages/node/nvm/versions/node/v24.18.0/bin/pi")))
 
 ;; (global-set-key (kbd "C-c s 3") #'my-open-sprite-tercero)
 
